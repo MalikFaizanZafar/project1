1888837b3e645ddd331acddaf4812a54b73526dd
MalikFaizanZafar <malikfaizanzafar1993@gmail.com>
2017-12-01 14:18:43 +0500

    routing done

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-index',
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
 })
 export class IndexComponent implements OnInit {
 
-  constructor(public router : Router) { }
+  constructor() { }
 
   ngOnInit() {
   }
